9206ebb7004534d15936201ca0bb405c798668d2
Liuding <he.2295@osu.edu>
2023-06-17 01:49:44 -0400

    add a test class

---
+++ cf_cpu/src/modules/test/test_out.cpp
@@ -0,0 +1,12 @@
+//
+// Created by hld on 6/16/23.
+//
+
+#include "test_out.h"
+
+namespace test {
+
+        void test_out::print(std::string str) {
+            std::cout << str << std::endl;
+        }
+} // test
---
+++ cf_cpu/src/modules/test/test_out.h
@@ -0,0 +1,19 @@
+//
+// Created by hld on 6/16/23.
+//
+
+#ifndef CF_CPU_TEST_OUT_H
+#define CF_CPU_TEST_OUT_H
+
+#include <string>
+#include <iostream>
+
+namespace test {
+    class test_out {
+    public:
+        void print(std::string str);
+    };
+
+} // test
+
+#endif //CF_CPU_TEST_OUT_H
