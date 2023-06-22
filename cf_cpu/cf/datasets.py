7daca0bff81eb8b1f45e8c819679f0037840db70
visuOwO <held18@outlook.com>
2023-06-22 03:25:58 -0400

    reformat datasets.py

--- cf_cpu/cf/datasets.py
+++ cf_cpu/cf/datasets.py
@@ -107,24 +107,27 @@ class ClickDataset(Dataset):
         return self.user_items_dic
 
 
-class SubClickDataset(ClickDataset):
-    def __init__(self):
-        Dataset().__init__()
+class SubClickDataset(Dataset):
+    def __init__(self, parent_dataset, start, end, rank=None):
+        super().__init__()
+        self.click_dataset = None
 
-    def split(self, parent_dataset, start, end):
-        Dataset().__init__()
-        self.c_class = cf_c.modules.datasets.ClickDataset
+        test = cf_c.modules.test.test_out()
+        self.c_class = None
 
         self.file_path = parent_dataset.file_path
         self.user_items_dic = {}
         for user_id in parent_dataset.get_user_items():
             if start <= user_id < end:
                 self.user_items_dic[user_id - start] = parent_dataset.get_user_items()[user_id]
+
         self.user_item_ids = []
         self.user_ids_dic = {}
         self.item_ids_dic = {}
         self.num_users = 0
         self.num_items = 0
+        self.rank = rank
+
         for user_id in self.user_items_dic:
             items = self.user_items_dic[user_id]
             for item in items:
@@ -132,6 +135,81 @@ class SubClickDataset(ClickDataset):
                     self.item_ids_dic[item] = item
                 self.user_item_ids.append([user_id, item])
             self.user_ids_dic[user_id] = user_id
+
+        self.max_his = parent_dataset.max_his
+        self.his_items = np.zeros((len(self.user_ids_dic), self.max_his), dtype=np.uint64)
+        self.masks = np.zeros((len(self.user_ids_dic), 1), dtype=np.uint64)
+
+        for user_id in self.user_items_dic:
+            items = self.user_items_dic[user_id]
+            if len(items) >= self.max_his:
+                user_his = random.sample(items, self.max_his)
+                self.his_items[user_id] = user_his
+                self.masks[user_id] = [self.max_his]
+            elif len(items) > 0:
+                user_his = items.copy()
+                user_his.extend([user_his[-1]] * (self.max_his - len(items)))
+                self.his_items[user_id] = user_his
+                self.masks[user_id] = [len(items)]
+        print(f'number of users: {len(self.user_ids_dic)}; number of items: {len(self.item_ids_dic)}; total samples: {len(self.user_item_ids)} ')
+        self.inherit_dataset_info(parent_dataset)
+
+    def inherit_dataset_info(self, parent_dataset):
+        self.num_users = parent_dataset.num_users
+        self.num_items = parent_dataset.num_items
+        user_ids = list(self.user_ids_dic.keys())
+        item_ids = list(self.item_ids_dic.keys())
+
+        max_user_id = max(user_ids)
+        min_user_id = min(user_ids)
+        max_item_id = max(item_ids)
+        min_item_id = min(item_ids)
+
+        if max_user_id - min_user_id + 1 != self.num_users:
+            print('Warning user_id is not continuous! ')
+
+        if max_item_id - min_item_id + 1 != self.num_items:
+            print('Warning item_id is not continuous! ')
+
+        print(f'number of users: {self.num_users}; min_user_id: {min_user_id}; max_user_id: {max_user_id}')
+        print(f'number of items: {self.num_items}; min_item_id: {min_item_id}; max_item_id: {max_item_id}')
+        print(f'total samples: {len(self.user_item_ids)} ')
+
+    def gen_dataset_info(self):
+        print(f'gen dataset info in process {self.rank} of {self.file_path}')
+        self.num_users = len(self.user_ids_dic)
+        self.num_items = len(self.item_ids_dic)
+        user_ids = list(self.user_ids_dic.keys())
+        item_ids = list(self.item_ids_dic.keys())
+        # user_ids.sort()
+        # item_ids.sort()
+        max_user_id = max(user_ids)
+        min_user_id = min(user_ids)
+        max_item_id = max(item_ids)
+        min_item_id = min(item_ids)
+        if max_user_id - min_user_id + 1 != self.num_users:
+            print('Warning user_id is not continuous! ')
+
+        if max_item_id - min_item_id + 1 != self.num_items:
+            print('Warning item_id is not continuous! ')
+
+        print(f'number of users: {self.num_users}; min_user_id: {min_user_id}; max_user_id: {max_user_id}')
+        print(f'number of items: {self.num_items}; min_item_id: {min_item_id}; max_item_id: {max_item_id}')
+        print(f'total samples: {len(self.user_item_ids)} ')
+
+    def update_config(self, config=None):
+        print('update config, init c_instance !!! ')
+        self.c_class = cf_c.modules.datasets.ClickDataset
+        config.num_users = self.num_users
+        config.num_items = self.num_items
+        config.train_size = len(self.user_item_ids)
+
+        test = cf_c.modules.test.test_out()
+
+        self.click_dataset = np.array(self.user_item_ids, dtype=np.uint64)
+        test.test(str(self.click_dataset.shape))
+        self.init_c_instance(click_dataset=self.click_dataset, historical_items=self.his_items, masks=self.masks)
+        self.c_instance.max_his = self.max_his
 
         self.gen_dataset_info()
 
